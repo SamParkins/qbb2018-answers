9dc8db071a1b011802c2401e0f2eecdb88c2a320
SamParkins <sparkin4@jhu.edu>
2018-08-28 19:37:36 -0400

    pyscript

---
+++ day2-homework/flyhw.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#make a new file with two columns
+
+import sys
+f = open( sys.argv[1] )
+
+
+for i, line in enumerate( f ):
+    if "DROME" in line and "FBgn" in line:
+        fields = line.rstrip("\r\n").split()
+        print(fields[3], "\t", fields[2])
+        
